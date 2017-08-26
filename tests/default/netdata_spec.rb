02971469f512ac103a36de3840b93bed531a0315
Avi Shefi <avishefi@users.noreply.github.com>
2017-08-27 02:15:25 +0300

    add netdata config tests, reorder tests and linting

--- tests/default/netdata_spec.rb
+++ tests/default/netdata_spec.rb
@@ -3,8 +3,11 @@
 netdata_user = 'netdata'
 netdata_group = 'netdata'
 netdata_directory = '/opt/netdata'
+netdata_conf_dir = '/etc/netdata'
+netdata_conf = netdata_conf_dir + '/netdata.conf'
 netdata_service = 'netdata'
-netdata_url = 'http://localhost:19999/'
+
+netdata_url = 'http://localhost:19999'
 
 
 ##
@@ -22,6 +25,12 @@ end
 describe directory(netdata_directory) do
   it { should be_directory }
 end
+describe directory(netdata_conf_dir) do
+  it { should be_directory }
+end
+describe file(netdata_conf) do
+  it { should exist }
+end
 
 describe service(netdata_service) do
   it { should be_installed }
@@ -35,5 +44,10 @@ end
 ##
 
 describe http(netdata_url) do
+  its('status') { should cmp 200 }
+end
+
+# test allmetrics endpoint
+describe http(netdata_url + '/api/v1/allmetrics?format=shell') do
   its('status') { should cmp 200 }
 end
