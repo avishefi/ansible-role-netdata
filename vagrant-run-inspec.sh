c13d27f2b059cb10f004530069a1c6898c8c2611
Avi Shefi <avishefi@users.noreply.github.com>
2017-08-27 03:22:20 +0300

    add inspec runner for Vagrant machine

---
+++ vagrant-run-inspec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+IFS='' _vagrant_ssh_config=$(vagrant ssh-config)
+_vagconf() {
+  echo "$_vagrant_ssh_config" | grep -E "$1\s" 2>/dev/null | awk '{print $2}' 2>/dev/null | tr -d '[:space:]'
+}
+
+set -x
+
+hostname=$(_vagconf 'HostName')
+username=$(_vagconf 'User')
+port=$(_vagconf 'Port')
+keyfile=$(_vagconf 'IdentityFile')
+
+inspec exec tests/default -t "ssh://${username}@${hostname}:${port}" -i "${keyfile}"
